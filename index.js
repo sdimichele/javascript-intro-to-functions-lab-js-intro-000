e91b71d82342d1e39d70b24b27a460a9423a2aad
dependable-network-8033 <sabrinadimichele1992@gmail.com>
2019-03-03 23:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function sayHiToGrandma(string) {
     return "I love you, too."
   } else if (string = string.toLowerCase) {
     return "I can\'t hear you!"
-  } else if (string = string.toUpperCase) { 
+  } else if (string = string.toUpperCase) {
     return "YES INDEED!"
-  
+
 
   }
 
